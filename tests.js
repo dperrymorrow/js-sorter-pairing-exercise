037cf9e826f7d205d908f54dcab60d443135f6db
David Morrow <dmorrow@newrelic.com>
2012-10-18 15:58:27 -0700

    adding the files

---
+++ tests.js
@@ -0,0 +1,36 @@
+/*global jQuery:true, equal:true, test:true, sorter:true, $:true*/
+/*jslint browser: true, white: true, vars: true, devel: true, bitwise: true, debug: true, nomen: true, sloppy: true, indent: 2*/
+
+test("not duplicating the tr elements", function () {
+  sorter($('#table'), 'num_key', 'DESC');
+  ok($('#table tr').length, 5);
+});
+
+test("sorting by numeric data key", 2, function() {
+  sorter($('#table'), 'num_key', 'DESC');
+  ok($('#table tr').first().data('num_key'), 1);
+    
+  sorter($('#table'), 'num_key', 'ASC');
+  ok($('#table tr').first().data('num_key'), 5);
+});
+
+test("sorting ASC and desc on strings", 2, function() {
+  sorter($('#table'), 'str_key', 'ASC' );
+  ok( $('#table tr').first().data('str_key'), 'Watermelon');
+  
+  sorter($('#table'), 'str_key', 'DESC');
+  ok( $('#table tr').first().data('str_key'), 'Apples');
+});
+
+////////////////// BONUS ROUND /////////////////////
+
+test("sorting ASC and desc on strings", 3, function() {
+  sorter($('#table'), 'text', 'ASC' );
+  ok($('#table tr').first().text(), 'Watermelon');
+  
+  sorter($('#table'), 'text', 'DESC');
+  ok( $('#table tr').first().text(), 'Apples');
+
+  sorter($('#table'));
+  ok( $('#table tr').first().text(), 'Apples');
+});
