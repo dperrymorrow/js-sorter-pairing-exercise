7fd0a0ac933224509d773c90d1584c912a16adb2
David Morrow <dmorrow@newrelic.com>
2012-10-18 17:14:16 -0700

    removing answer

--- code.js
+++ code.js
@@ -2,23 +2,5 @@
 /*jslint browser: true, white: true, vars: true, devel: true, bitwise: true, debug: true, nomen: true, sloppy: true, indent: 2*/
 
 function sorter($table, sortOn, direction) {
-  // default to text as direction
-  direction = direction || 'ASC';
-  // default to text for sortOn
-  sortOn = sortOn || 'text';
-  // accepts lowercase
-  direction = direction.toUpperCase();
-  // an array to store our values in
-  var values = [];
-  // iterate over the tr elements and pluck the values
-  $table.find('tr').each(function () {
-    values.push($(this).data(sortOn));
-  });
-  // sort the values asc or desc
-  values = (direction === 'DESC') ? values.sort() : values.sort().reverse();
-  // move the tr elements to match the values array
-  for (var i = 0; i < values.length; i++) {
-    var $tr = $table.find('tr[data-' + sortOn + '=' + values[i] + ']');
-    $table.append($tr);
-  }
+  // your code here...
 }
